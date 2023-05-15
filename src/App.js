5949468d67f902fe34ec44d47fb6b3f5f3ba7ffd
IvanTaranenko <vanokalipsi@gmail.com>
2023-05-15 23:53:03 +0300

    add Header with styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
+import Header from "./components/Header/Header";
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello,world</h1>
-        </div>
+      <Header />
     );
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { AppBar, Toolbar, Typography, IconButton, InputBase, Box } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+
+import useStyles from './styles';
+
+const Header = () => {
+    const classes = useStyles();
+
+    return (
+        <AppBar position="static">
+            <Toolbar className={classes.toolbar}>
+                <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h5" className={classes.title}>
+                    Travel Advisor
+                </Typography>
+                <Box display="flex">
+                    <Typography variant="h5" className={classes.title}>
+                        Explore new places
+                    </Typography>
+                    {/* <Autocomplete> */}
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon />
+                        </div>
+                        <InputBase
+                            placeholder="Search..."
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput,
+                            }}
+                        />
+                    </div>
+                    {/* </Autocomplete> */}
+                </Box>
+            </Toolbar>
+        </AppBar>
+    );
+}
+
+export default Header;
