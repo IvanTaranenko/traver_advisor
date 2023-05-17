0bfed0752887ba10af7ffcf084032c26010ddea4
IvanTaranenko <vanokalipsi@gmail.com>
2023-05-17 12:56:26 +0300

    add few features in List components

---
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+
+import useStyles from './styles.js';
+
+const PlaceDetails = ({place}) => {
+    return (
+        <h1>{place.name}</h1>)
+
+}
+
+export default PlaceDetails;
